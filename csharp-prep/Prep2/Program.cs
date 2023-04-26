acd45ea33fb2f7af0622f50fbcd1fec8224fd1d0
codebyhannah <firekatburns@gmail.com>
2023-04-26 17:20:49 -0600

    added code to calculate grade and print a message

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter grade percentage: ");
+        string userInput = Console.ReadLine();
+        int numGrade = int.Parse(userInput);
+
+        string letter = "";
+
+        if (numGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (numGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (numGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (numGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        Console.WriteLine($"Your grade is: {letter}");
+        if (numGrade >= 70)
+        {
+            Console.WriteLine("Congratulations, you pass!");
+        }
+        else 
+        {
+            Console.WriteLine("You fail.");
+        }
+
+        
+
+
     }
 }
